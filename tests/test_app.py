3e166631eb545dd6204536c4f9c3925661029d9f
mrkhan9573 <mrkhan9573@gmail.com>
2026-08-02 17:26:02 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,23 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import app
 
 client = app.test_client()
+
 
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
 
+
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
 
-def test_employee():
-    response = client.get("/employees/1")
+
+def test_employees():
+    response = client.get("/employees")
     assert response.status_code == 200
